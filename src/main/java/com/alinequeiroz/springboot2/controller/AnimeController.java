ea823feecaf19d1c9cc49bcf88c1ecf3b808aa83
Aline Queiroz <Aline.queiroz@capgemini.com>
2023-06-07 09:16:26 -0300

    Classe Anime e AnimeController

---
+++ src/main/java/com/alinequeiroz/springboot2/controller/AnimeController.java
@@ -0,0 +1,44 @@
+package com.alinequeiroz.springboot2.controller;
+
+import com.alinequeiroz.springboot2.domain.Anime;
+import com.alinequeiroz.springboot2.service.AnimeService;
+import com.alinequeiroz.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("animes")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
+
+    @GetMapping
+    public ResponseEntity<List<Anime>> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> deleteAnime(@PathVariable Long id){
+        animeService.deleteAnime(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
