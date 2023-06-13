508c657aedac82a7b83e5b900d86f45eb1206e69
Aline Queiroz <Aline.queiroz@capgemini.com>
2023-06-13 15:54:45 -0300

    Configurando o banco de dados mySQL

--- src/main/java/com/alinequeiroz/springboot2/domain/Anime.java
+++ src/main/java/com/alinequeiroz/springboot2/domain/Anime.java
@@ -2,10 +2,22 @@ package com.alinequeiroz.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
